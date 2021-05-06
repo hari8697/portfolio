54de7fd21ee7d2383177638ea5884b94111e42e2
hari8697 <hari8697@gmail.com>
2021-05-06 21:04:49 +0530

    first commit

---
+++ components/Landing/landing.tsx
@@ -0,0 +1,9 @@
+import styled from "styled-components"
+
+const H1 = styled.h1`
+  color: blueviolet;
+`
+
+export default function index() {
+  return <H1 className="">Hello World</H1>
+}
---
+++ pages/index.tsx
@@ -0,0 +1,25 @@
+import Head from "next/head"
+import Portfolio from "../components/Landing/landing"
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Lato&family=Montserrat:wght@400;700&display=swap"
+          rel="stylesheet"
+        />
+        <title>DeathSpace Design</title>
+        <meta name="description" content="Portfolio made by Hari Krishnan" />
+        <link rel="icon" href="/doge.ico" />
+      </Head>
+
+      <main>
+        <Portfolio />
+      </main>
+
+      <footer></footer>
+    </div>
+  )
+}
