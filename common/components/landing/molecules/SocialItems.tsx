7effc23944f35cbeb830107fa2bd2452b8c71cc3
hari8697 <hari8697@gmail.com>
2021-11-18 23:23:47 +0530

    feat: Social links landing

--- common/components/landing/molecules/SocialItems.tsx
+++ common/components/landing/molecules/SocialItems.tsx
@@ -1,12 +1,18 @@
 const SocialItems = () => {
-  const socialItemsArr = Array.from(Array(4).keys())
+  // const socialItemsArr = Array.from(Array(4).keys())
+  const socialItemsArr = [
+    "mailto:deathspacedesign@gmail.com",
+    "https://twitter.com/deathspace_",
+    "https://www.instagram.com/deathspace.design/",
+    "https://discord.gg/AWuYKDnQ",
+  ]
   // let iconSize = 18
-  const socialItems = socialItemsArr.map((item) => {
+  const socialItems = socialItemsArr.map((item, index) => {
     return (
-      <div className="icon_wrapper" key={item}>
-        <div className="social_icon">
-          <img src={`/landing/social/${item + 1}.svg`}></img>
-        </div>
+      <div className="icon_wrapper" key={index}>
+        <a className="social_icon" href={item}>
+          <img src={`/landing/social/${index + 1}.svg`}></img>
+        </a>
       </div>
     )
   })
