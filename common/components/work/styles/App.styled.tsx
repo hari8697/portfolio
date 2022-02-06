0277eab7f6a9947d4e5d76c7c9e49ab676efc496
hari8697 <hari8697@gmail.com>
2022-02-06 23:13:53 +0530

    style: Hide nav buttons on mobile

--- common/components/work/styles/App.styled.tsx
+++ common/components/work/styles/App.styled.tsx
@@ -469,7 +469,11 @@ export const SwiperContainerStyled = styled(motion.div)`
 
   .swiper-button-next,
   .swiper-button-prev {
-    opacity: 0.2;
+    opacity: 0;
+
+    @media ${device.laptop} and (orientation: landscape) {
+      opacity: 0.2;
+    }
     transition: opacity 200ms ease-out;
     padding: 1rem 2rem;
     &:hover,
